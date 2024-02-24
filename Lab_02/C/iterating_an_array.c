fc5b0ed0889aa3659e25aed546892cfd55edc55e
Mashrur Safir Shabab <Shabab.ahmed2000sa@gmail.com>
2024-02-24 16:14:29 +0600

    Create iterating_an_array.c

---
+++ Lab_02/C/iterating_an_array.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+
+int main(){
+
+       int arr[5] = {1,2,3,4,5};
+
+       for(int i=0; i<5; i++){
+           printf("arr[%d]:%d\n", i, arr[i]);
+       }
+
+	return 0;
+}
